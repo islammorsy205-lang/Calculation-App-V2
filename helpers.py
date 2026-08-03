adc648cecacc21bb0a788f71ce5a5828c9bfcd76
ISLAM MORSY <islammorsy205@gmail.com>
2026-08-03 11:07:55 +0300

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -5,7 +5,6 @@ import re
 from PIL import Image, ImageChops
 import streamlit as st
 import numpy as np
-from config import STRUTS_DB
 
 def crop_white_margins(img):
     if img.mode != "RGB":
@@ -76,7 +75,25 @@ def get_best_image_match(subject, system, image_list):
             max_score, best_idx = score, idx
     return best_idx
 
+def get_strut_allowable(name, length):
+    from config import STRUTS_DB
+    data = STRUTS_DB.get(name)
+    if not data: return 0.0
+    if 'allow' in data: return data['allow']
+    if 'pts' in data:
+        pts = data['pts']
+        keys = sorted(pts.keys())
+        if length <= keys[0]: return pts[keys[0]]
+        if length >= keys[-1]: return pts[keys[-1]]
+        for i in range(len(keys)-1):
+            k1, k2 = keys[i], keys[i+1]
+            if k1 <= length <= k2:
+                if k1 == k2: return pts[k1]
+                return pts[k1] + (pts[k2] - pts[k1]) * (length - k1) / (k2 - k1)
+    return 0.0
+
 def get_valid_struts(req_len, mode="wind"):
+    from config import STRUTS_DB
     valid = []
     for k, v in STRUTS_DB.items():
         if v['min'] <= req_len <= v['max']:
@@ -97,16 +114,17 @@ def get_valid_struts(req_len, mode="wind"):
             if "PPH" in n_up: score = 0
             elif "PPS" in n_up: score = 1
         else:
-            if "TILT" in n_up or "MPP" in n_up: score = 0
+            if "TILT" in n_up or "MPP6" in n_up: score = 0
             elif "PPS" in n_up: score = 1
             elif "PPH" in n_up: score = 2
-        
+            
+        # إعطاء أولوية ضعيفة جداً للأنواع المذكورة لإرجاعها للخلف
+        if any(x in n_up for x in ["MPP1", "MPP2", "MPP3", "MPP4", "MPP5"]): 
+            score += 20
         match = re.search(r'\d+', name.split()[0])
-        if match and int(match.group()[-1]) in [1, 3, 5]:
+        if match and int(match.group()[-1]) in [1, 3, 5]: 
             score += 20
-        if "X1" in n_up or "X3" in n_up or "X5" in n_up:
-            score += 20
-        if "X4" in n_up:
+        if "X4" in n_up: 
             score += 20
             
         return (score, name)
